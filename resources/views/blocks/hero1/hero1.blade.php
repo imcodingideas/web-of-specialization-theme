18208ef4de4b44fef703793c0892d03a4f03dcd6
Joseph Chambers <joseph@michael-chambers.com>
2023-04-18 17:41:44 -0600

    block hero1 fields

--- resources/views/blocks/hero1/hero1.blade.php
+++ resources/views/blocks/hero1/hero1.blade.php
@@ -1,1 +1,24 @@
-<div class='text-5xl'>hello {{ $hello }}</div>
+@if (!empty($title))
+  <div class="">{{ $title }}</div>
+@endif
+
+@if (!empty($text))
+  <div class="">{!! nl2br(htmlspecialchars($text)) !!}</div>
+@endif
+
+@if (!empty($link1))
+  <a class="" href="<?php echo $link1['url']; ?>" target="<?php echo $link1['target']; ?>">
+    <?php echo htmlspecialchars($link1['title']); ?>
+  </a>
+@endif
+
+
+@if (!empty($link2))
+  <a class="" href="<?php echo $link2['url']; ?>" target="<?php echo $link2['target']; ?>">
+    <?php echo htmlspecialchars($link2['title']); ?>
+  </a>
+@endif
+
+@if (!empty($image))
+  {!! wp_get_attachment_image($image['id'], 'full', false, ['class' => '']) !!}
+@endif
